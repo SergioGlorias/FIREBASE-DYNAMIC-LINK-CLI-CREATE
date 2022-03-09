a52bef17b8e93c8348bfba0c9959e557143c10e5
Sérgio Glórias <sergio@serginho.dev>
2022-03-09 00:57:33 +0000

    encortador de link

---
+++ index.js
@@ -0,0 +1,31 @@
+import { config } from "dotenv";
+config();
+
+import axios from "axios";
+import validUrl from "valid-url";
+
+import readline from "readline";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Insira o link que quer encortar: ", async (link) => {
+    if (!validUrl.isWebUri(link)) return console.log("Error: Você deve inserir um link válido!") && rl.close();
+
+    try {
+        let res = await axios.post(`https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=${process.env.FIREBASE_DYNAMIC_LINK_KEY}`, {
+            dynamicLinkInfo: {
+                domainUriPrefix: process.env.FIREBASE_DYNAMIC_LINK_DOMAIN,
+                link: link
+            }
+        });
+        
+        console.log(`Link encurtado: ${res.data.shortLink}`);
+    } catch (error) {
+        console.error(error.response.data)
+    } finally {
+        rl.close();
+    }
+})
